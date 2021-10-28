77e594502a794e380ea9fd33622a1cd7cc4ef7b8
Fellipe Santos <fellipe.santos@wdev.int>
2021-10-28 14:49:41 -0300

    Alteração do SQL e Alteração do Layout da página de Contato

--- src/main/resources/db/db.sql
+++ src/main/resources/db/db.sql
@@ -1,21 +1,76 @@
-CREATE TABLE `contact_type` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `contact_type` varchar(60) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+-- MySQL Workbench Forward Engineering
 
-CREATE TABLE `contacts` (
-  `ID` int NOT NULL AUTO_INCREMENT,
-  `PHONE_NUMBER` varchar(13) NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
-CREATE TABLE `user` (
-  `user_id` int NOT NULL AUTO_INCREMENT,
-  `user_name` varchar(60) DEFAULT NULL,
-  `user_gender` varchar(6) DEFAULT NULL,
-  `user_country` varchar(20) DEFAULT NULL,
-  `user_about_you` varchar(100) DEFAULT NULL,
-  `user_mailing_list` char(1) DEFAULT NULL,
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900
+-- -----------------------------------------------------
+-- Schema struts
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema struts
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `struts` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `struts` ;
+
+-- -----------------------------------------------------
+-- Table `struts`.`contact_type`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `struts`.`contact_type` (
+  `ID` INT NOT NULL AUTO_INCREMENT,
+  `CONTACT_TYPE` VARCHAR(60) NULL DEFAULT NULL,
+  PRIMARY KEY (`ID`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 4
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `struts`.`contacts`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `struts`.`contacts` (
+  `ID` INT NOT NULL AUTO_INCREMENT,
+  `PHONE_NUMBER` VARCHAR(13) NOT NULL,
+  PRIMARY KEY (`ID`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 4
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `struts`.`user`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `struts`.`user` (
+  `user_id` INT NOT NULL AUTO_INCREMENT,
+  `user_name` VARCHAR(60) NULL DEFAULT NULL,
+  `user_gender` VARCHAR(6) NULL DEFAULT NULL,
+  `user_country` VARCHAR(20) NULL DEFAULT NULL,
+  `user_about_you` VARCHAR(100) NULL DEFAULT NULL,
+  `user_mailing_list` CHAR(1) NULL DEFAULT NULL,
+  PRIMARY KEY (`user_id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 3
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `struts`.`endereco`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `struts`.`endereco` (
+  `ID_ENDERECO` INT NOT NULL AUTO_INCREMENT,
+  `LOGRADOURO` VARCHAR(400) NULL,
+  `CEP` VARCHAR(8) NULL,
+  `COMPLEMENTO` VARCHAR(45) NULL,
+  `ESTADO` VARCHAR(2) NULL,
+  `PAIS` VARCHAR(45) NULL,
+  PRIMARY KEY (`ID_ENDERECO`))
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
--- src/main/webapp/contact.jsp
+++ src/main/webapp/contact.jsp
@@ -5,40 +5,55 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<script src="https://kit.fontawesome.com/94257b76a9.js" crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/94257b76a9.js"
+	crossorigin="anonymous"></script>
 <title>User Contacts</title>
 <s:head />
 <style type="text/css">
 @import url(style.css);
+table {
+    border-collapse: separate;
+    border-spacing: 0;
+}
+ 
+table{
+    border: 1px solid black;
+    border-radius: 5px;
+    -moz-border-radius: 5px;
+    padding: 5px;
+}
 </style>
 </head>
 <body>
-
 	<s:form action="listContactType">
 		<s:select label="Tipos de contatos" headerKey="-1"
 			headerValue="Selecione seu contato" list="contactTypeList"
 			name="contactType" listKey="id" listValue="contactType" />
 	</s:form>
-	<div>
-		<s:form action="saveOrUpdate">
-			<s:textfield name="phoneNumber" id="phone_with_ddd" label="Celular" />
-			<s:submit value="Ok" />
-		</s:form>
-	</div>
-	<div>
-		<s:form action="saveOrUpdate">
-			<s:textfield name="phoneNumber" label="Email" />
-			<s:submit value="Ok" />
-		</s:form>
-	</div>
-	<div>
-		<s:form action="saveOrUpdate">
-			<s:textfield name="phoneNumber" label="Endereço" placeholder="Rua"/>
-			<s:textfield name="phoneNumber" placeholder="Número"/>
-			<s:textfield name="phoneNumber" placeholder="Bairro"/>
-			<s:textfield name="phoneNumber" placeholder="CEP"/>
-			<s:submit value="Ok" />
-		</s:form>
-	</div>
+	<table>
+		<tr>
+			<s:form action="saveOrUpdate">
+				<s:textfield name="phoneNumber" id="phone_with_ddd" label="Celular" />
+				<s:submit value="Ok" />
+			</s:form>
+		</tr>
+		<tr>
+			<s:form action="saveOrUpdate">
+				<s:textfield name="phoneNumber" label="Email" />
+				<s:submit value="Ok" />
+			</s:form>
+		</tr>
+		<tr>
+			<s:form action="saveOrUpdate" label="Endereco">
+				<s:textfield name="phoneNumber" label="Endereço" placeholder="Rua" />
+				<s:textfield name="phoneNumber" placeholder="Número" />
+				<s:textfield name="phoneNumber" placeholder="Bairro" />
+				<s:textfield name="phoneNumber" placeholder="CEP" />
+				<s:submit value="Ok" />
+			</s:form>
+		</tr>
+
+	</table>
+
 </body>
 </html>
